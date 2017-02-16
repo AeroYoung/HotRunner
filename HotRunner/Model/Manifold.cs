c962ed9497f8d5771d9a3e4cf5f8874f19b4388e
AeroYoung <saucadcam@126.com>
2017-02-16 14:09:19 +0800

    how?

--- HotRunner/Model/Manifold.cs
+++ HotRunner/Model/Manifold.cs
@@ -39,8 +39,6 @@ namespace HotRunner
             {
                 boolstatus = swDoc.Extension.SelectByID2("Line1", "SKETCHSEGMENT", (lines[i].Start.X+lines[i].End.X)/2, (lines[i].Start.Y + lines[i].End.Y) / 2, 0, true, 1, null, 0);
             }
-            //boolstatus = swDoc.Extension.SelectByID2("Line1", "SKETCHSEGMENT", -0.023950509454303562, 0.00944726436442634, 0.013460865810254692, false, 1, null, 0);
-            //boolstatus = swDoc.Extension.SelectByID2("Line6", "SKETCHSEGMENT", 0.0056611571475044253, 0.0069563930633148238, -0.00047254587326235262, true, 1, null, 0);
             boolstatus = swDoc.SketchManager.SketchOffset(0.022, true, true, false, false, true);
             //swDoc.ClearSelection2(true);
             //boolstatus = swDoc.EditRebuild3();//退出草图并重建图形
