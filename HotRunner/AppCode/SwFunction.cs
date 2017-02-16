b090e591351d46dd5f5b6fac93fa728e29909e61
AeroYoung <saucadcam@126.com>
2017-02-16 11:42:46 +0800

    ActivateSelectedFeature

--- HotRunner/AppCode/SwFunction.cs
+++ HotRunner/AppCode/SwFunction.cs
@@ -10,6 +10,12 @@ namespace HotRunner
 {
     public static class SwFunction
     {
+        /// <summary>
+        /// 从数组中获得Line
+        /// </summary>
+        /// <param name="value">数组</param>
+        /// <param name="count">数量</param>
+        /// <returns></returns>
         public static List<Line> GetLines(double[] value, int count)
         {
             List<Line> result = new List<Line>();
@@ -19,7 +25,7 @@ namespace HotRunner
                 double[] a = new double[12];
                 for (int j = 0; j < 12; j++)
                 {
-                    a[j] = value[count * 12 + j];
+                    a[j] = value[(count - 1) * 12 + j];
                 }
                 result.Add(new Line(a));
             }
--- HotRunner/Model/Manifold.cs
+++ HotRunner/Model/Manifold.cs
@@ -24,17 +24,16 @@ namespace HotRunner
             ModelDoc2 swDoc = null;
             bool boolstatus = false;
             swDoc = ((ModelDoc2)(swApp.ActiveDoc));
-            swDoc.ClearSelection2(true);
-            boolstatus = swDoc.Extension.SelectByID2(fet.Name, "SKETCH", 0, 0, 0, false, 0, null, 0);
-            swDoc.ActivateSelectedFeature();
+            //swDoc.ClearSelection2(true);
+            //swDoc.SetPickMode();
+            //boolstatus = swDoc.Extension.SelectByID2(fet.Name, "SKETCH", 0, 0, 0, false, 0, null, 0);
+            //swDoc.ActivateSelectedFeature();
 
-            //获得所有线段
-            double[] lines = sketch.GetLines2((short)swCrossHatchFilter_e.swCrossHatchExclude);
-            int count = sketch.GetLineCount2((short)swCrossHatchFilter_e.swCrossHatchExclude);
-            
-            
-            //boolstatus = swDoc.Extension.SelectByID2("Line1", "SKETCHSEGMENT", -0.023950509454303562, 0.00944726436442634, 0.013460865810254692, true, 0, null, 0);
-            //boolstatus = swDoc.Extension.SelectByID2("Line6", "SKETCHSEGMENT", 0.0056611571475044253, 0.0069563930633148238, -0.00047254587326235262, true, 0, null, 0);
+            //////获得所有线段
+            ////double[] linesValue = sketch.GetLines2((short)swCrossHatchFilter_e.swCrossHatchExclude);
+            ////int count = sketch.GetLineCount2((short)swCrossHatchFilter_e.swCrossHatchExclude);
+            ////List<Line> lines = SwFunction.GetLines(linesValue,count);
+
             //swDoc.ClearSelection2(true);
             //boolstatus = swDoc.Extension.SelectByID2("Line1", "SKETCHSEGMENT", -0.023950509454303562, 0.00944726436442634, 0.013460865810254692, false, 1, null, 0);
             //boolstatus = swDoc.Extension.SelectByID2("Line6", "SKETCHSEGMENT", 0.0056611571475044253, 0.0069563930633148238, -0.00047254587326235262, true, 1, null, 0);
