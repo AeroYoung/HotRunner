85ee2c813a94053d0f289fc0bf87e4d869c56414
AeroYoung <saucadcam@126.com>
2017-02-16 09:02:10 +0800

    hanlder

--- HotRunner/View/BtnManifoldPage.cs
+++ HotRunner/View/BtnManifoldPage.cs
@@ -110,17 +110,17 @@ namespace HotRunner
 
         public void OnSelectionboxCalloutCreated(int Id)
         {
-            //throw new NotImplementedException();
+            ////throw new NotImplementedException();
         }
 
         public void OnSelectionboxCalloutDestroyed(int Id)
         {
-            //throw new NotImplementedException();
+            ////throw new NotImplementedException();
         }
 
         public void OnSelectionboxFocusChanged(int Id)
         {
-            //throw new NotImplementedException();
+            ////throw new NotImplementedException();
         }
 
         /// <summary>
@@ -133,8 +133,13 @@ namespace HotRunner
             //MessageBox.Show(selection1.GetSelectedItemsCount().ToString());
             if (Id == selection1ID)
             {
-                ModelDoc2 swDoc = ((ModelDoc2)(iSwApp.ActiveDoc));
-                
+                //ModelDoc2 swDoc = ((ModelDoc2)(iSwApp.ActiveDoc));
+                if (Count > 0)
+                {
+                    short[] items = selection1.GetSelectedItems();
+                    MessageBox.Show(items[0].ToString());
+                    
+                }
             }
         }
 
@@ -142,17 +147,18 @@ namespace HotRunner
 
         public void AfterActivation()
         {
-            //throw new NotImplementedException();
+            ////throw new NotImplementedException();
         }
 
         public void AfterClose()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public int OnActiveXControlCreated(int Id, bool Status)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return -1;
         }
 
         public void OnButtonPress(int Id)
@@ -162,7 +168,7 @@ namespace HotRunner
 
         public void OnCheckboxCheck(int Id, bool Checked)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         /// <summary>
@@ -176,137 +182,145 @@ namespace HotRunner
 
         public void OnComboboxEditChanged(int Id, string Text)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnComboboxSelectionChanged(int Id, int Item)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnGainedFocus(int Id)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnGroupCheck(int Id, bool Checked)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnGroupExpand(int Id, bool Expanded)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public bool OnHelp()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return true;
         }
 
         public bool OnKeystroke(int Wparam, int Message, int Lparam, int Id)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return true;
         }
 
         public void OnListboxRMBUp(int Id, int PosX, int PosY)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnListboxSelectionChanged(int Id, int Item)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnLostFocus(int Id)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public bool OnNextPage()
         {
-            throw new NotImplementedException();
+            return true;
+            //throw new NotImplementedException();
         }
 
         public void OnNumberboxChanged(int Id, double Value)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnNumberBoxTrackingCompleted(int Id, double Value)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnOptionCheck(int Id)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnPopupMenuItem(int Id)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnPopupMenuItemUpdate(int Id, ref int retval)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public bool OnPreview()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return true;
         }
 
         public bool OnPreviousPage()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return true;
         }
 
         public void OnRedo()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
         
         public void OnSliderPositionChanged(int Id, double Value)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnSliderTrackingCompleted(int Id, double Value)
         {
-            throw new NotImplementedException();
+            ////throw new NotImplementedException();
         }
 
         public bool OnSubmitSelection(int Id, object Selection, int SelType, ref string ItemText)
         {
-            throw new NotImplementedException();
+            ////throw new NotImplementedException();
+            return true;
         }
 
         public bool OnTabClicked(int Id)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return true;
         }
 
         public void OnTextboxChanged(int Id, string Text)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnUndo()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnWhatsNew()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public int OnWindowFromHandleControlCreated(int Id, bool Status)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return 0;
         }
 
         #endregion
