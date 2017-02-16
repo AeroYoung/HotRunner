b876afcf963985219e00d57d96c7aa062c068a38
AeroYoung <saucadcam@126.com>
2017-02-16 15:53:37 +0800

    enum sketch

--- HotRunner/Model/Manifold.cs
+++ HotRunner/Model/Manifold.cs
@@ -21,11 +21,11 @@ namespace HotRunner
         public void Commit(Sketch sketch)
         {
             Feature fet = (Feature)sketch;
-            ModelDoc2 swDoc = null;
-            bool boolstatus = false;
-            swDoc = ((ModelDoc2)(swApp.ActiveDoc));
-            
-            boolstatus = swDoc.Extension.SelectByID2(fet.Name, "SKETCH", 0, 0, 0, false, 0, null, 0);
+            ModelDoc2 swDoc = (ModelDoc2)swApp.ActiveDoc;
+            SelectionMgr swSelMgr = (SelectionMgr)swDoc.SelectionManager;
+
+            bool boolstatus = swDoc.Extension.SelectByID2(fet.Name, "SKETCH", 0, 0, 0, false, 0, null, 0);
+
             swDoc.EditSketch();
             swDoc.ClearSelection2(true);
 
@@ -34,12 +34,28 @@ namespace HotRunner
             int count = sketch.GetLineCount2((short)swCrossHatchFilter_e.swCrossHatchExclude);
             List<Line> lines = SwFunction.GetLines(linesValue, count);
 
-            swDoc.ClearSelection2(true);
-            for (int i = 0; i < count; i++)
+            //swDoc.ClearSelection2(true);
+            List<SketchSegment> segments = new List<SketchSegment>();
+            SelectData data = swSelMgr.CreateSelectData();
+            data.Mark = 1000;
+            EnumSketchSegments enumSegments = sketch.IEnumSketchSegments();
+            if (enumSegments != null)
             {
-                boolstatus = swDoc.Extension.SelectByID2("Line1", "SKETCHSEGMENT", (lines[i].Start.X + lines[i].End.X) / 2, (lines[i].Start.Y + lines[i].End.Y) / 2, 0, true, 1, null, 0);
+                SketchSegment segment;
+                int next = 1;
+                enumSegments.Next(1,out segment,ref next);
+                
+                while (segment != null)
+                {
+                    segments.Add(segment);
+                    segment = null;
+                    enumSegments.Next(1, out segment, ref next);
+                }
             }
-            boolstatus = swDoc.Extension.SelectByID2("Line1", "SKETCHSEGMENT", 0, 0, 0, true, 1, null, 0);
+            for (int i = 0; i < segments.Count; i++)
+            {
+                boolstatus = segments[i].Select(true);
+            }
             boolstatus = swDoc.SketchManager.SketchOffset(0.022, true, true, false, false, true);
             //swDoc.ClearSelection2(true);
             //boolstatus = swDoc.EditRebuild3();//退出草图并重建图形
